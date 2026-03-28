c987b17a1a0344db77232b62f0006467ffcb5cb4
Bruno Lisboa <brunolisboacontato@gmail.com>
2026-03-27 23:05:03 -0300

    bg transparent and gruvbox added

--- init.lua
+++ init.lua
@@ -25,9 +25,10 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- add your plugins here
+      { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true,},
       {"rebelot/kanagawa.nvim"},
       {'nvim-telescope/telescope.nvim', version = '*', dependencies = {'nvim-lua/plenary.nvim', 
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },}},
+{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },}},
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -35,7 +36,57 @@ require("lazy").setup({
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
-vim.cmd("colorscheme kanagawa-wave")
+-- gruvbox theme config:
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = true,
+})
+
+-- kanagawa theme config:
+require('kanagawa').setup({
+    compile = false,             -- enable compiling the colorscheme
+    undercurl = true,            -- enable undercurls
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true},
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = true,         -- do not set background color
+    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    colors = {                   -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    },
+    overrides = function(colors) -- add/modify highlights
+        return {}
+    end,
+    theme = "wave",              -- Load "wave" theme
+    background = {               -- map the value of 'background' option to a theme
+        dark = "wave",           -- try "dragon" !
+        light = "lotus"
+    },
+})
+vim.cmd("colorscheme kanagawa")
 
 vim.keymap.set('n', '<f5>', ':!python3 %', { silent = false })
 vim.keymap.set('n', '<f6>', ':term python3 %', { silent = false })
